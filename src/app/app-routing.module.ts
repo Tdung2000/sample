ad80f1743cf70e7c9a2e403c3f17a76d33158784
dungnt <dungliemtuc@gmail.com>
2022-08-18 06:27:03 +0700

    routing to rxjs-fantasy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RxjsComponent } from './views/rxjs/rxjs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'rxjs', component: RxjsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
